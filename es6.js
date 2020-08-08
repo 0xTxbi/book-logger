5ff09b89df3a1dfbfed3fe14fbcd3bbcb7210df7
TechieJossy <techieJossy@users.noreply.github.com>
2020-08-08 12:13:38 +0100

    Conversion of syntax to ES6

---
+++ es6.js
@@ -0,0 +1,41 @@
+class Book {
+	
+	constructor(title, author, isbn) {
+
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+
+	}
+
+}
+
+class UI {
+
+	addBookToList() {
+
+
+	}
+
+
+	showErrorNotification() {
+
+
+	}
+
+
+	showSuccessNotification() {
+
+
+	}
+
+	clearFields() {
+
+
+	}
+
+	deleteBook() {
+
+		
+	}
+}
